b8b1f8e220cfb8cfb063856f8fcf8879a27a0fce
shivani patil <sspatil.khakanar@gmail.com>
2021-11-11 17:16:25 +0530

    productMemoryandStorage

---
+++ Solution1/MyShop.Core/Models/product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+namespace MyShop.Core.Models
+{
+    public class product
+    { 
+        public string Id { get; set; }
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Range(0,1000)]
+        public decimal price { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+
+        public product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+    }
+}
